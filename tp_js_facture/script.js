27d73123e91d60f55f1ccb19be05c1138f75eb84
Reymond <julesreymond.jr@gmail.com>
2024-11-04 16:39:07 +0100

    v1jsfacture

---
+++ tp_js_facture/script.js
@@ -0,0 +1,27 @@
+function resultat(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)* Number(b);
+    t3.value = c;
+    var d = document.getElementById("t5").value;
+    var e = document.getElementById("t6").value;
+    var f = Number(d)* Number(e);
+    t7.value = f;
+    var i = document.getElementById("t9").value;
+    var j = document.getElementById("t10").value;
+    var k = Number(i)* Number(j);
+    t11.value = k;
+
+    var l = document.getElementById("t3").value;
+    var m = document.getElementById("t7").value;
+    var n = document.getElementById("t11").value;
+    var o = Number(l)+ Number(m)+ Number(n);
+    t13.value = o;
+}
+function réinitialiser(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)- Number(b);
+
+    t3.value = c;
+}
